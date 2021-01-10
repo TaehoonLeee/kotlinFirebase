6c878ab9f7c3f473bb3ebcd7fe43db7c1449c20c
leetaehoon <lyc9290@gmail.com>
2021-01-10 19:13:18 +0900

    register activity..
    I implement inserting data at fire realtime database

--- app/src/main/java/com/example/kotlinfirebase/login.kt
+++ app/src/main/java/com/example/kotlinfirebase/login.kt
@@ -1,6 +1,9 @@
 package com.example.kotlinfirebase
 
+import android.content.Intent
+import android.hardware.display.DisplayManager
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.kotlinfirebase.databinding.ActivityLoginBinding
@@ -10,6 +13,13 @@ class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.btnRegister.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val intent = Intent(this@login, register::class.java)
+                startActivity(intent)
+            }
+        })
     }
 
     private lateinit var binding: ActivityLoginBinding
---
+++ app/src/main/java/com/example/kotlinfirebase/register.kt
@@ -0,0 +1,44 @@
+package com.example.kotlinfirebase
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.kotlinfirebase.databinding.ActivityRegisterBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class register : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register)
+
+        database = Firebase.database.reference
+        binding.btnDone.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                register()
+            }
+        })
+    }
+
+    fun register() {
+        var email = binding.etEmail.text.toString()
+        var password = binding.etPassword.text.toString()
+
+        val user = User(email, password)
+
+        database.child("users").setValue(user)
+    }
+
+    data class User(
+        var email: String? = "",
+        var password: String? = ""
+    )
+
+
+
+    private lateinit var binding : ActivityRegisterBinding
+    private lateinit var database : DatabaseReference
+}
