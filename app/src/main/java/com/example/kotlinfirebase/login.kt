b2216bef4cc02802bb9f99210bda40edd93184c3
leetaehoon <lyc9290@gmail.com>
2021-01-10 18:31:41 +0900

    login activity data binding

--- app/src/main/java/com/example/kotlinfirebase/login.kt
+++ app/src/main/java/com/example/kotlinfirebase/login.kt
@@ -1,4 +1,16 @@
 package com.example.kotlinfirebase
 
-class login {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.kotlinfirebase.databinding.ActivityLoginBinding
+
+class login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+    }
+
+    private lateinit var binding: ActivityLoginBinding
 }
